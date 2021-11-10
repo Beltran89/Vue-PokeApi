391cf2ac2e6970be26dc45e60acd9ea6678ce4ab
rbeltran <rbeltran@insfera.com>
2021-11-10 15:51:10 +0100

    Vue Poke API

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './css/animations.css'
+import './css/styles.css'
+
 createApp(App).use(router).mount('#app')
